8630627765fb6f7cb445600dd7cdd4cfd0d7ef04
maha fariz <mahafariz20@gmail.com>
2026-02-03 00:32:24 +0100

    Create product_sign_checker.py

---
+++ product_sign_checker.py
@@ -0,0 +1,8 @@
+nmbr1 = int(input("entrer un nombre : "))
+nmbr2 = int(input("entrer un nombre : "))
+if nmbr1 * nmbr2 > 0:
+    print("produit positif")
+elif nmbr1 * nmbr2 < 0:
+    print("produit negativ")
+else :
+    print("produit nul")
